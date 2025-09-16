79202a6109af0bf05e97504192801b566a930f23
Luan <luansilva_2@hotmail.com>
2025-09-16 20:27:14 -0300

    1

---
+++ app.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+
+const loginRoute = require('./routes/loginRoute');
+
+app.use(loginRoute);
+
+app.listen(3000, function(){
+    console.log('APP rodando na porta 3000');
+})
